2e284f59d5e8875769b2eda57e56aac10bed31ad
Azen0n <jack.2013.ghost@gmail.com>
2022-10-06 15:27:39 +0500

    Add datatypes and article parsing

---
+++ datatypes.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import TypedDict
+import re
+import time
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+
+
+@dataclass
+class Node:
+    letter: str
+    parent: Node | None
+    children: list[Node] = field(default_factory=list)
+    words: list[Word] = field(default_factory=list)
+
+
+@dataclass
+class WordTree:
+    root: Node
+
+
+class Words(TypedDict):
+    """Singleton's internal word dictionary."""
+    stemmed_word: str
+    cls: Word
+
+
+class WordSingleton(type):
+    """Splitting sentences in words results in creating new object for 
+    each word. This kind of singleton will return existing word with 
+    new form-sentence record in it or create new word if it doesn't exist."""
+    _words: Words = {}
+    def __call__(cls, stemmed_word: str, word_form: str, sentence: Sentence):
+        if stemmed_word in cls._words:
+            cls.__append_form_and_sentence(stemmed_word, word_form, sentence)
+        else:
+            cls._words[stemmed_word] = super(
+                WordSingleton, cls).__call__(stemmed_word, word_form, sentence)
+        return cls._words[stemmed_word]
+    
+    def __append_form_and_sentence(cls, stemmed_word: str, word_form: str,
+                                        sentence: Sentence):
+        if word_form in cls._words[stemmed_word].forms:
+            cls._words[stemmed_word].forms[word_form].append(sentence)
+        else:
+            cls._words[stemmed_word].forms[word_form] = [sentence]
+        cls._words[stemmed_word]._count += 1
+        
+
+
+class WordFormSentence(TypedDict):
+    """Word base is stemmed word, all other word forms kept in 
+    form-sentences dictionary."""
+    word_form: str
+    sentences: list[Sentence]
+
+
+@dataclass
+class Word(metaclass=WordSingleton):
+    """Word.
+    
+    * text — word base.
+    * forms — dictionary with word forms and list of sentences 
+    with these forms.
+    * count — number of occurences of all forms with this base
+    (across all articles)."""
+    _stemmed_word: str
+    forms: WordFormSentence = field(init=False)
+    _count: str = 0
+    
+    def __init__(self, stemmed_word: str, word_form: str, sentence: Sentence):
+        """:param stemmed_word: word base (stemmed using nltk 
+        SnowballStemmer).
+        :param word_form: any form with base stemmed_word.
+        :param sentence: Sentence object with this word form.
+        """
+        self._stemmed_word = stemmed_word
+        self.forms = {word_form: [sentence]}
+        self._count += 1
+    
+    def __str__(self) -> str:
+        return self._stemmed_word
+    
+    @property
+    def text(self) -> str:
+        return self._stemmed_word
+    
+    @property
+    def count(self) -> int:
+        return self._count
+
+
+@dataclass
+class Sentence:
+    """Sentence.
+    
+    * article — Article object with this sentence.
+    * text — preprocessed sentence string.
+    * words — list of words in this sentence.
+    """
+    article: Article
+    text: str
+    words: list[Word] = field(default_factory=list)
+    
+    def __post_init__(self):
+        self.__sentence_preprocessing()
+        self.__split_sentence_into_words()
+    
+    def __sentence_preprocessing(self):
+        """Remove special characters and extra whitespaces from sentence."""
+        self.text = re.sub(r'[^a-zA-Zа-яА-Я\s]', '', self.text)
+        self.text = re.sub(r'\s+', ' ', self.text)
+        self.text = self.text.strip()
+    
+    def __split_sentence_into_words(self):
+        """Split sentence into list of stemmed words."""
+        words = word_tokenize(self.text)
+        stemmer = SnowballStemmer('russian')
+        for word in words:
+            word = Word(stemmer.stem(word), word, self)
+            if word not in self.words:
+                self.words.append(word)
+
+
+@dataclass
+class Article:
+    """Article.
+    
+    * authors — list of author names of article in format 'И.О. Фамилия'
+    * title — article title (ALL CAPS).
+    * text — preprocessed article text.
+    * sentences — list of sentences in this article.
+    """
+    authors: list[str]
+    title: str
+    text: str
+    sentences: list[Sentence] = field(default_factory=list)
+    
+    def __post_init__(self):
+        start_time = time.time()
+        self.__flatten_article_title()
+        self.__text_preprocessing()
+        self.__split_into_sentences()
+        end_time = time.time()
+        self.__log(end_time - start_time)
+    
+    def __flatten_article_title(self):
+        """Remove newline characters and repeating whitespaces from 
+        article titles."""
+        self.title = re.sub(r'\n', '', self.title)
+        self.title = re.sub(r'\s{2,}', ' ', self.title)
+        self.title = self.title.strip()
+    
+    def __text_preprocessing(self):
+        """Remove newline characters and 'рис. #' references."""
+        self.text = self.text.lower()
+        self.text = re.sub('\n', '', self.text)
+        self.text = re.sub('рис\.\s?\d\.?', '', self.text)
+
+    def __split_into_sentences(self):
+        """Split article text into sentences."""
+        sentences = sent_tokenize(self.text)
+        for sentence in sentences:
+            self.sentences.append(Sentence(self, sentence))
+
+    def __log(self, time: float):
+        print(f'Article completed: {self.title} ({time:.2f}s)')
+        print(f'Number of sentences: {len(self.sentences)}')
+        word_count = 0
+        for sentence in self.sentences:
+            word_count += len(sentence.words)
+        print(f'Number of unique words: {word_count}\n')
---
+++ main.py
@@ -0,0 +1,56 @@
+import re
+
+import fitz
+import nltk
+
+from datatypes import Article
+
+
+def main():
+    txt_path = 'file.txt'
+    nltk.download('punkt')
+    
+    with open(txt_path, encoding='utf8') as f:
+        text = f.read()
+    articles = split_text_into_articles(text)
+    print(1)
+
+
+def read_pdf(path: str, save_txt_path: str = None) -> str:
+    with fitz.open(path) as document:
+        text = ''
+        for page in document:
+            text += page.get_text()
+    if save_txt_path:
+        with open(save_txt_path, 'w', encoding='utf8') as f:
+            f.write(text)
+    return text
+
+
+def split_text_into_articles(text: str) -> list[Article]:
+    """Returns list of Article objects, consisting of
+    list of authors, title and main text without 'СПИСОК ЛИТЕРАТУРЫ'.
+    """
+    pattern = re.compile(r"""(?:\d+\s?)\n   # page number (ignored)
+                         ((?:.+\n){1,5})    # authors and their universities
+                         (?:УДК:?\s+[\d]+\.?[\d]+.+\n)  # УДК (ignored)
+                         ((?:.+\n){1,5}(?=Аннотация))   # article title
+                         (?:Аннотация\.?\:?)    # counts as first sentence
+                                                # so ignored
+                         """, re.X)
+    authors_pattern = r'[А-ЯA-Z]\. ?[А-ЯA-Z]\.? [А-Яа-я]+'
+    splitted_text = re.split(pattern, text)
+    articles = []
+    try:
+        for i in range(1, len(splitted_text), 3):
+            authors = re.findall(authors_pattern, splitted_text[i])
+            title = splitted_text[i + 1]
+            raw_text = re.split('СПИСОК ЛИТЕРАТУРЫ', splitted_text[i + 2])[0]
+            articles.append(Article(authors, title, raw_text))
+    except IndexError:
+        raise IndexError('Wrong split occurred. Adjust regex pattern.')
+    return articles
+    
+
+if __name__ == '__main__':
+    main()
